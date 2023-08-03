d3fa7edd17f32c732abab94adafc25aede6b548d
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2023-08-03 16:25:56 +0300

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -19,15 +19,13 @@ chrome.webNavigation.onCommitted.addListener(details=>{
   if(transition.includes(details.transitionType)) vkplayFunc(details);
 },filter);
 
-
 function vkplayTools(){
-
   setTimeout(()=>{
+    
     // баллы
     let points = document.querySelector('[class^="PointActions_root"]');
 
     if(points != null){
-
       function pointsAction(){
         let pointsButton = document.querySelector('[class^="PointActions_buttonBonus"]');
         if(pointsButton != null) pointsButton.click();
@@ -51,7 +49,6 @@ function vkplayTools(){
       let heartsStatus = document.querySelector('[class*="LikeButton_iconLiked"]');
       if(heartsStatus === null) heartsButton.click();
     };
-
 
   },1000);
 };
