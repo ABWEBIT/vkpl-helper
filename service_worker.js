0b110d0a1e089c39a2724ad8eaf4a65f165fac26
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2023-08-04 23:14:48 +0300

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -23,32 +23,54 @@ function vkplayTools(){
   setTimeout(()=>{
     
     // баллы
-    let points = document.querySelector('[class^="PointActions_root"]');
+    chrome.storage.sync.get(['loot']).then((result)=>{
+      if(result.loot != 'off' && !chrome.runtime.lastError){
 
-    if(points != null){
-      function pointsAction(){
-        let pointsButton = document.querySelector('[class^="PointActions_buttonBonus"]');
-        if(pointsButton != null) pointsButton.click();
+        let points = document.querySelector('[class^="PointActions_root"]');
+
+        if(points != null){
+          function pointsAction(){
+            let pointsButton = document.querySelector('[class^="PointActions_buttonBonus"]');
+            if(pointsButton != null) pointsButton.click();
+          };
+          pointsAction();
+
+          let observer = new MutationObserver(pointsObserver);
+
+          function pointsObserver(mutations){
+            for(let mutation of mutations){
+              if(mutation.type === 'childList') pointsAction();
+            }
+          };
+
+          observer.observe(points, {childList: true});
+        };
+
       };
-      pointsAction();
-
-      let observer = new MutationObserver(pointsObserver);
-
-      function pointsObserver(mutations){
-        for(let mutation of mutations){
-          if(mutation.type === 'childList') pointsAction();
-        }
-      };
-
-      observer.observe(points, {childList: true});
-    };
+    });
 
     // сердечко
-    let heartsButton = document.querySelector('[class^="LikeButton_container"]');
-    if(heartsButton != null){
-      let heartsStatus = document.querySelector('[class*="LikeButton_iconLiked"]');
-      if(heartsStatus === null) heartsButton.click();
-    };
+    chrome.storage.sync.get(['like']).then((result)=>{
+      if(result.like != 'off' && !chrome.runtime.lastError){
+        let heartsButton = document.querySelector('[class^="LikeButton_container"]');
+        if(heartsButton != null){
+          let heartsStatus = document.querySelector('[class*="LikeButton_iconLiked"]');
+          if(heartsStatus === null) heartsButton.click();
+        };
+      };
+    });
 
   },1000);
 };
+
+chrome.storage.sync.get(['loot']).then((result)=>{
+  if(result.loot == null || chrome.runtime.lastError){
+    chrome.storage.sync.set({loot:'on'});
+  }
+});
+
+chrome.storage.sync.get(['like']).then((result)=>{
+  if(result.like == null || chrome.runtime.lastError){
+    chrome.storage.sync.set({like:'on'});
+  }
+});
